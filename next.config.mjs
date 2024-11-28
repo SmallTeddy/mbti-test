c85db721bcd828e66548becb249782b4f79fcc07
SmallTeddy <zhangzeming1212@gmail.com>
2024-11-28 14:33:00 +0800

    chore: 部署配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  router: {
+    base: "/mbti-test/",
+  },
+  output: "export",
+};
 
 export default nextConfig;
